eeceb99dadef459eba7ba0261932a17c9d43b667
Sohaib Rajput <srajput@payactiv.com.pk>
2024-01-24 06:55:11 -0800

    end of section 4

--- Program.cs
+++ Program.cs
@@ -18,6 +18,7 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -28,9 +29,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
 
-app.UseAuthorization();
+//app.UseHttpsRedirection();
+
+//app.UseAuthorization();
+
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 app.MapControllers();
 
